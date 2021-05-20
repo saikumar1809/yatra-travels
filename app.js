a136c93cd1955cff141ac53606e8f6d2e5d20fab
saikumar1809 <saikumarkumarpandhula@gmail.com>
2021-05-20 08:39:46 +0530

    Added a commment

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 
+//Start express app
 const app = express();
 //set security HTTP headers
 app.set('view engine', 'pug');
